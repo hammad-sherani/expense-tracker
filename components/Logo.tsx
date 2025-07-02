7738105646537c165c1d436381c59352264e77c9
Hammad Khan <hammad@blinkswag.com>
2025-07-02 17:27:43 +0500

    first commit

---
+++ components/Logo.tsx
@@ -0,0 +1,17 @@
+import { BadgeDollarSign } from 'lucide-react'
+import Link from 'next/link'
+import React from 'react'
+
+function Logo() {
+    return (
+        <Link href='/' className='flex items-center gap-2'>
+            <BadgeDollarSign className='stroke h-11 w-11 stroke-amber-500 stroke-[1.5]' />
+            <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-transparent text-3xl font-bold">
+                ExpenseTracker
+            </p>
+
+        </Link>
+    )
+}
+
+export default Logo
