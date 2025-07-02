5edf47eaecdb0f47163326f353e0e398a65df70e
Hammad Khan <hammad@blinkswag.com>
2025-07-02 19:05:59 +0500

    install shadcn and add theme

---
+++ components/providers/RootProviders.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { ThemeProvider } from "next-themes";
+import React from "react";
+
+function RootProviders({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <ThemeProvider
+            attribute={"class"}
+            defaultTheme="dark"
+            enableSystem={true}
+            disableTransitionOnChange={false}
+        >
+            {children}
+        </ThemeProvider>
+    )
+
+
+}
+
+export default RootProviders;
